0db32404935a792911a36d011516fb8e3441259d
9musharrafkhan <239400805+9musharrafkhan@users.noreply.github.com>
2026-05-15 12:03:56 +0200

    matlab

--- 01_Linear_Cantilever/Matlab/stiff_global.m
+++ 01_Linear_Cantilever/Matlab/stiff_global.m
@@ -15,19 +15,3 @@ for n1 = 1:size(Tdof,1)
 end
 
 end
-%% Global stiffness matrix
-function [K] = stiff_global(Tdof, Geo, E, ny)
-
-% initalize
-K = zeros(max(Tdof(:)));
-
-for n1 = 1:size(Tdof,1)
-
-    % Element stiffness matrix
-    [k] = stiff(Geo(:,:,n1), E, ny);
-
-    % Assembling
-    K(Tdof(n1, :), Tdof(n1, :)) = K(Tdof(n1, :), Tdof(n1, :)) + k;
-end
-
-end
